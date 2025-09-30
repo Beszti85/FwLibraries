1097a6380dadb9a67c03e7f3832ee440b0191aca
Beszti85 <csababesztercei@gmail.com>
2025-09-30 23:04:04 +0200

    correct header due to c++

--- Inc/pc_uart_handler.h
+++ Inc/pc_uart_handler.h
@@ -13,10 +13,6 @@
 #include <string.h>
 #include "main.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 typedef enum
 {
   PCUART_CONNECT = 0u,
@@ -26,6 +22,9 @@ typedef enum
   PCUART_NUM_OF_CMD
 } PCUART_ServiceCommand_e;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 uint8_t PCUART_ProcessRxCmd( uint8_t* ptrRxBuffer, uint8_t* ptrTxBuffer );
 
